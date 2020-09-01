05264015119bc8adee079c8125211a1d769033f0
ACHIYA96 <39017907+ACHIYA96@users.noreply.github.com>
2020-09-01 13:09:25 +0530

    Update status_message_fn.py

--- tobrot/plugins/status_message_fn.py
+++ tobrot/plugins/status_message_fn.py
@@ -84,7 +84,7 @@ async def status_message_f(client, message):
     if msg == "":
         msg = "🤷‍♂️ No Active, Queued or Paused TORRENTs \n I'm just chilling here 😎"
 
-    currentTime = TimeFormatter((time.time() - BOT_START_TIME))
+    currentTime = time.strftime("%H:%M:%S", time.gmtime(time.time() - BOT_START_TIME))
     total, used, free = shutil.disk_usage(".")
     total = humanbytes(total)
     used = humanbytes(used)
